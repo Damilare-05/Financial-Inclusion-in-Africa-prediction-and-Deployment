2a7d6c38489a2d3bdf6e9b86a0aba3dcfc35ab00
Talabi Damilare Hannah <damilaretalabi9@gmail.com>
2024-10-08 14:40:11 +0100

    Update FinancialInclusionDeployment.py

--- FinancialInclusionDeployment.py
+++ FinancialInclusionDeployment.py
@@ -16,7 +16,7 @@ import joblib
 st.title('Bank Account Prediction in East Africa')
 
 # Load the trained model
-model = joblib.load("C:/Users/Dami/Downloads/model.pkl")  
+model = joblib.load("model.pkl")  
 
 # Input features from the user
 st.header('Input Features')
